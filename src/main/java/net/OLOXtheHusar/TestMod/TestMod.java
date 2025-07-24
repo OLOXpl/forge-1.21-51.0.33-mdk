21e6401d335c8a1f96015994cdccc2ccdae30125
OLOXpl <oliwierwoj01@gmail.com>
2025-07-24 19:54:03 +0200

    .

--- src/main/java/net/OLOXtheHusar/TestMod/TestMod.java
+++ src/main/java/net/OLOXtheHusar/TestMod/TestMod.java
@@ -1,6 +1,9 @@
 package net.OLOXtheHusar.TestMod;
 
 import com.mojang.logging.LogUtils;
+import net.OLOXtheHusar.TestMod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,7 +32,7 @@ public class TestMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,6 +46,9 @@ public class TestMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.OLOXITE);
+        }
 
     }
 
